215705593569352dea327216f857c8219433e993
Austin Howard <cnfishead@hotmail.com>
2024-12-07 10:20:15 -0500

    setting up initial page layout

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,31 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    API_URL: process.env.API_URL,
+    ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
+    ENV: process.env.NODE_ENV,
+    // ENV: "development",
+  },
+  //Redirect / to /dash
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+      {
+        source: "/login",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
+  sassOptions: {
+    silenceDeprecations: ["legacy-js-api"],
+  },
+  assetPrefix: process.env.ENV === "production" ? "https://api.shepherdcms.org" : "",
 };
 
 export default nextConfig;
