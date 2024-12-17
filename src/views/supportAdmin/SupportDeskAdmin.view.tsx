780cd10106e4a2d6bf5cdeba7905ec7197627117
Austin Howard <cnfishead@hotmail.com>
2024-12-17 09:20:40 -0500

    fixing build errors

--- src/views/supportAdmin/SupportDeskAdmin.view.tsx
+++ src/views/supportAdmin/SupportDeskAdmin.view.tsx
@@ -1,49 +1,25 @@
-import React from "react";
-import styles from "./SupportDeskAdmin.module.scss";
-import { Tabs } from "antd";
-import AnimatedDiv from "@/components/animatedDiv/AnimatedDiv.UI";
-import { AnimatePresence } from "framer-motion"; 
-import SupportGroups from "./views/support_groups/SupportGroups.component";
-
-interface Tab {
-  key: string;
-  title: string;
-  content: React.ReactNode;
-}
+import React from 'react';
+import styles from './SupportDeskAdmin.module.scss';
+import { Tabs, TabsProps } from 'antd';
+import SupportGroups from './views/support_groups/SupportGroups.component';
 
 const SupportDeskAdmin = () => {
   const tabs = [
     {
-      key: "0",
-      title: "Support Groups",
-      content: <SupportGroups />,
+      key: '0',
+      label: 'Support Groups',
+      children: <SupportGroups />,
     },
-    {
-      key: "1",
-      title: "Agents",
-      content: <></>,
-    },
-  ] as Tab[];
+  ] as TabsProps['items'];
 
   return (
     <div className={styles.container}>
-       <Tabs defaultActiveKey="0" type="card">
-        {tabs.map((tab) => (
-          <Tabs.TabPane tab={tab.title} key={tab.key}>
-            <AnimatePresence mode="wait">
-              <AnimatedDiv
-                transitionType="fade"
-                duration={0.5}
-                key={`switchableView-${tab.key}`}
-                type="whileInView"
-                className={styles.tabsContainer}
-              >
-                {tab.content}
-              </AnimatedDiv>
-            </AnimatePresence>
-          </Tabs.TabPane>
-        ))}
-      </Tabs>
+      <Tabs
+        className={styles.tabs}
+        items={tabs}
+        animated={{ inkBar: true, tabPane: true }}
+        type="card"
+      />
     </div>
   );
 };
